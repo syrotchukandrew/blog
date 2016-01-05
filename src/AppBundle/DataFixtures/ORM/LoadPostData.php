6a65c6c42899263c0a61d2d9d04f00f7850cf310
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-01-05 19:12:26 +0200

    added fixtures

--- src/AppBundle/DataFixtures/ORM/LoadPostData.php
+++ src/AppBundle/DataFixtures/ORM/LoadPostData.php
@@ -8,9 +8,14 @@ use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\Post;
 use AppBundle\Entity\Comment;
 use Faker\Factory;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadTeamData extends AbstractFixture implements OrderedFixtureInterface
+class LoadPostData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
+    /** @var ContainerInterface */
+    private $container;
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
@@ -19,14 +24,18 @@ class LoadTeamData extends AbstractFixture implements OrderedFixtureInterface
             $post = new Post();
             $post->setTitle($faker->sentence);
             $post->setContent($faker->realText($maxNbChars = 3000, $indexSize = 2));
+            $post->setCreated(new \DateTime('now'));
+            $post->setUpdated(new \DateTime('now'));
+            $post->setSlug($post->getTitle());
             $manager->persist($post);
             $this->addReference("{$id}", $post);
             $id = $id + 1;
 
             $rand = rand(3,7);
-            for ($i = 0; $i < $rand; $i++) {
+            for ($j = 0; $j < $rand; $j++) {
                 $comment = new Comment();
                 $comment -> setContent($faker->realText($maxNbChars = 500, $indexSize = 2));
+                $comment->setCreated(new \DateTime('now'));
                 $comment-> setPost($post);
                 $post->getComments()->add($comment);
                 $manager->persist($comment);
@@ -37,6 +46,14 @@ class LoadTeamData extends AbstractFixture implements OrderedFixtureInterface
 
 
         $manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 
     public function getOrder()
---
+++ src/AppBundle/DataFixtures/ORM/LoadTagData.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Tag;
+//use Faker\Factory;
+
+class LoadTagData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $titles = ['sport', 'nature', 'fishing', 'finance', 'religion', 'hobby',
+            'gossip', 'technology', 'money', 'resorts'];
+        //$faker = Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $tag = new Tag();
+            $tag->setTitle($titles[$i]);
+            $tag->setCreated(new \DateTime('now'));
+            $tag->setUpdated(new \DateTime('now'));
+            $tag->setSlug($titles[$i]);
+            $manager->persist($tag);
+            $manager->flush();
+
+            $arrayId = null;
+            $arrayId = array();
+
+            while (count($arrayId) < 8) {
+
+                $id = rand(1, 50);
+                if ((array_search($id, $arrayId)) === false) {
+                    $arrayId[] = $id;
+                    $postFromBase = $this->getReference("$id");
+                    $postFromBase->getTags()->add($tag);
+                    $tag->getPosts()->add($postFromBase);
+                }
+            }
+            $manager->flush();
+        }
+    }
+
+    public function getOrder()
+    {
+        return 2;
+    }
+}
