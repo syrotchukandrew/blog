60dbb4d2c41816a4a3a69a7993af7d7b7a2129fb
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-01-14 21:00:27 +0200

    added scrutinazer

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,6 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, 200);
-        //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Symfony', 'Symfony');
     }
 }
