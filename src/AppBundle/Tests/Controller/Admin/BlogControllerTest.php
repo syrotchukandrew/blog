ea7bcf205857df962310f5554b269410466c2508
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-01-15 11:16:18 +0200

    added quasytest next silly test

---
+++ src/AppBundle/Tests/Controller/Admin/BlogControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Tests\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BlogControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, 200);
+        $this->assertContains('', '');
+    }
+}
