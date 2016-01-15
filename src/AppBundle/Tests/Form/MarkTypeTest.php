421d5e8657cc23c9272e4e7699635b050d61cbca
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-01-15 10:20:18 +0200

    added quasytest next silly test

---
+++ src/AppBundle/Tests/Form/MarkTypeTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Tests\AppBundle\Form\Type;
+
+use AppBundle\Form\MarkType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class MarkTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $formData = array(
+            'mark' => '5',
+        );
+        $form = $this->factory->create(MarkType::class);
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals(1, 1);
+        $view = $form->createView();
+        $children = $view->children;
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
