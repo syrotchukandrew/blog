7fbe252dd4a6513b75913fac6fd823c72799fb75
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-01-15 11:14:14 +0200

    added quasytest next silly test

--- src/AppBundle/Tests/Controller/BlogControllerTest.php
+++ src/AppBundle/Tests/Controller/BlogControllerTest.php
@@ -4,7 +4,7 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class BlogControllerTest extends WebTestCase
 {
     public function testIndex()
     {
