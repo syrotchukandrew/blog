03ef18dc5cc051999de41e81abbd7fcdfdfc8c5c
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-01-15 10:17:08 +0200

    added quasytest next silly test

--- src/AppBundle/Tests/Form/CommentTypeTest.php
+++ src/AppBundle/Tests/Form/CommentTypeTest.php
@@ -1,27 +1,27 @@
 <?php
 namespace Tests\AppBundle\Form\Type;
 
-use AppBundle\Entity\Post;
-use AppBundle\Form\PostType;
+use AppBundle\Entity\Comment;
+use AppBundle\Form\CommentType;
 use Symfony\Component\Form\Test\TypeTestCase;
 
-class PostTypeTest extends TypeTestCase
+class CommentTypeTest extends TypeTestCase
 {
     public function testSubmitValidData()
     {
-        /*$formData = array(
-            'title' => 'test',
+        $formData = array(
+            'content' => 'Andrew Syrotchuk',
         );
-        $form = $this->factory->create(PostType::class);
-        $object = new Post();
-        $object->setTitle('test');
+        $form = $this->factory->create(CommentType::class);
+        $object = new Comment();
+        $object->setContent('Andrew Syrotchuk');
         $form->submit($formData);
-        //$this->assertTrue($form->isSynchronized());
+        $this->assertTrue($form->isSynchronized());
         $this->assertEquals($object, $form->getData());
         $view = $form->createView();
         $children = $view->children;
         foreach (array_keys($formData) as $key) {
             $this->assertArrayHasKey($key, $children);
-        }*/
+        }
     }
 }
