dca4dd7a41ef6e0987a817aff7845d33e20e9af2
syrotchukandrew <andrewsyrotchuk@gmail.com>
2016-01-28 21:17:34 +0200

    added twig extension

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -9,7 +9,36 @@
 namespace AppBundle\Twig;
 
 
-class AppExtension
+class AppExtension extends \Twig_Extension
 {
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('dots3', array($this, 'dots3'), array('is_safe' => array('html'))),
+            );
+    }
 
+    public function dots3($content, $limit = 25)
+    {
+        $words = explode(' ', (trim($content)));
+        $countWords = count($words);
+        if ($countWords < $limit) {
+            $lim = $countWords;
+        } else {
+            $lim = $limit;
+        }
+        $words[($lim-1)] .= '...<em>Read More</em>...';
+        $strResult = '';
+        for ($i = 0; $i < $lim; $i++) {
+            $strResult .= $words[$i].' ';
+        }
+
+        return $strResult;
+    }
+
+
+    public function getName()
+    {
+        return 'app_extension';
+    }
 }
